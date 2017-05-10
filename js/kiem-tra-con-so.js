0291ef777ecf50bb73c41c1a13340a1807b473f0
minhhathanhsang <thanhsang.nguyen04041993@gmail.com>
2017-05-10 15:56:45 +0700

    dua file len

---
+++ js/kiem-tra-con-so.js
@@ -0,0 +1,19 @@
+$(function()
+{
+  
+  $("#st1,#st2").focusout(function(){
+     var regex = /^[0-9]+$/;    // allow only numbers [0-9] 
+     if( !regex.test($(this).val()) ) {
+      $(this).attr('title','nhap vao so nguyen');
+      $(this).focus();
+      $("#checkbox-tinhtoan").prop('checked',false);
+      $("#checkbox-tinhtoan").attr('DISABLED',true);
+     }
+     else
+     {
+         $(this).removeAttr('title');
+         $("#checkbox-tinhtoan").removeAttr('DISABLED');
+     }
+  });
+   
+});
