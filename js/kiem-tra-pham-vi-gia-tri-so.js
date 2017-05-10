e4242d7962f8f10bf1d6ae0227a03107f1f19b7b
minhhathanhsang <thanhsang.nguyen04041993@gmail.com>
2017-05-10 16:12:09 +0700

    Update kiem-tra-pham-vi-gia-tri-so.js

--- js/kiem-tra-pham-vi-gia-tri-so.js
+++ js/kiem-tra-pham-vi-gia-tri-so.js
@@ -1,16 +1,1 @@
-$(function()
-{
-  $("#st1-pt,#st1-pt-ptp,#st2-pt,#st2-pt-ptp").focusout(function(){
-     if($(this).val()<$(this).attr("min") || $(this).val()>$(this).attr("max"))
-     {
-         $(this).focus();
-         $("#checkbox-tinhtoan").prop('checked',false);
-         $("#checkbox-tinhtoan").attr('DISABLED',true);
-     }
-     else
-     {
-         $("#checkbox-tinhtoan").removeAttr('DISABLED');
-     }
-  });
-   
-});
+
